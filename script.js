19f074189cacc9da40f4e280223067c4aa7a4768
Mukund Chaturvedi <124137083+mukund2988@users.noreply.github.com>
2024-07-25 13:40:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function searchUser() {
   const url ="https://instagram-scraper-api2.p.rapidapi.com/v1/search_users?search_query=" +username.value;
   const request = new XMLHttpRequest();
   request.open("GET", url, true);
-  request.setRequestHeader("x-rapidapi-key","65a75442e0mshba2d2bc0555706ep15a1e8jsn8e32cab806c7");
+  request.setRequestHeader("x-rapidapi-key","your-api-key");
   request.setRequestHeader("x-rapidapi-host","instagram-scraper-api2.p.rapidapi.com");
   request.onload = callBackResponse;
   request.send();
